ec5e5fe85d0a54a73c73dafdece18a090c19d397
hashiehh44-dev <hashiehh44@gmail.com>
2026-08-13 22:51:02 +0530

    Test

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -1,65 +1,14 @@
-import { Webhook } from "svix";
-import connectToDatabase from "@/lib/Database";
-import UserModel from "@/models/User.Model";
-import Configs from "@/config/Configs";
-import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    try {
-        const wh = new Webhook(Configs.getClerkSigninSecretKey());
+    console.log("🔥 CLERK WEBHOOK HIT");
 
-        const headerPayload = await headers();
+    const body = await request.text();
 
-        const svixHeaders = {
-            "svix-id": headerPayload.get("svix-id"),
-            "svix-timestamp": headerPayload.get("svix-timestamp"),
-            "svix-signature": headerPayload.get("svix-signature"),
-        };
+    console.log("📦 BODY:", body);
 
-        // IMPORTANT: Get raw body for Svix verification
-        const body = await request.text();
-
-        const { data, type } = wh.verify(body, svixHeaders);
-
-        const userData = {
-            _id: data.id,
-            name: `${data.first_name || ""} ${data.last_name || ""}`.trim(),
-            email: data.email_addresses?.[0]?.email_address,
-            image: data.image_url,
-        };
-
-        await connectToDatabase();
-
-        switch (type) {
-            case "user.created":
-                await UserModel.create(userData);
-                break;
-
-            case "user.updated":
-                await UserModel.findByIdAndUpdate(data.id, userData);
-                break;
-
-            case "user.deleted":
-                await UserModel.findByIdAndDelete(data.id);
-                break;
-
-            default:
-                break;
-        }
-
-        return Response.json({
-            message: "Event Received",
-        });
-    } catch (error) {
-        console.error("Clerk webhook error:", error);
-
-        return Response.json(
-            {
-                message: "Webhook verification failed",
-            },
-            {
-                status: 400,
-            }
-        );
-    }
+    return NextResponse.json({
+        success: true,
+        message: "Webhook reached Next.js",
+    });
 }
---
+++ models/Chat.Model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            required: true
+        },
+        message: [
+            {
+                role: { type: String, required: true },
+                content: { type: String, required: true },
+                timestamp: { type: Number, required: true}
+            }
+        ],
+        userId: { type: String, required: true }
+    },
+    { timestamps: true }
+);
+
+
+const ChatModel = mongoose.models.chat || mongoose.model("user", ChatSchema);
+
+export default ChatModel;
